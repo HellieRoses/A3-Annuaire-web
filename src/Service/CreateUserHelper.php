357f82b4ed8a355720f7064d91fc1fd0ef0fdae2
romain <romain.touze.pro@gmail.com>
2024-09-23 11:34:36 +0200

    feat command : extract interface of CreateUserHelper.php

--- src/Service/CreateUserHelper.php
+++ src/Service/CreateUserHelper.php
@@ -4,7 +4,7 @@ namespace App\Service;
 
 use App\Repository\UtilisateurRepository;
 
-class CreateUserHelper
+class CreateUserHelper implements CreateUserHelperInterface
 {
 
     public function __construct(
---
+++ src/Service/CreateUserHelperInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Service;
+
+interface CreateUserHelperInterface
+{
+    public function verifyPassword(?string $password): bool;
+
+    public function verifyLogin(?string $login): bool;
+
+    public function verifyEmail(?string $email): bool;
+
+    public function verifyCode(?string $code): bool;
+}
